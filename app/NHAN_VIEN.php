274a6cda0aff114beaf72b7d6aedd5528928373d
nguyenthiy <63966829+nguyenthiy@users.noreply.github.com>
2020-05-01 12:53:57 +0700

    Update NHAN_VIEN.php

--- app/NHAN_VIEN.php
+++ app/NHAN_VIEN.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class NHAN_VIEN extends Model
 {
     //
+        protected $table = "NHAN_VIEN";
+    public function DON_DAT_HANG(){
+        return $this->hasMany('app\DON_DAT_HANG','ID_NV','ID_NV');
+    }
+    public function LOAI_NHAN_VIEN(){
+        return $this->belongsTo('app\LOAI_NHAN_VIEN','ID_NV','ID_NV');
+    }
 }
