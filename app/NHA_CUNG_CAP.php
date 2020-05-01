8501e15b03adad6dbd1446ccf59ecc67cc83ee7c
nguyenthiy <63966829+nguyenthiy@users.noreply.github.com>
2020-05-01 13:06:03 +0700

    Update NHA_CUNG_CAP.php

--- app/NHA_CUNG_CAP.php
+++ app/NHA_CUNG_CAP.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class NHA_CUNG_CAP extends Model
 {
     //
+        protected $table = "NHA_CUNG_CAP";
+    public function LO(){
+        return $this->hasMany('app\LO','ID_NCC','ID_NCC');
+    }
 }
