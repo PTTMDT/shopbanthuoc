e6ce18c749950054cae96e72000a8fa03ee7b2fa
nguyenthiy <63966829+nguyenthiy@users.noreply.github.com>
2020-05-01 12:53:22 +0700

    Update KHUYEN_MAI.php

--- app/KHUYEN_MAI.php
+++ app/KHUYEN_MAI.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class KHUYEN_MAI extends Model
 {
     //
+        protected $table = "KHUYEN_MAI";
+    public function LOAI_KHUYEN_MAI(){
+        return $this->belongsTo('app\LOAI_KHUYEN_MAI','ID_KM','ID_KM');
+    }
+    public function DON_DAT_HANG(){
+        return $this->hasMany('app\DON_DAT_HANG','ID_KM','ID_KM');
+    }
+    public function THUOC(){
+        return $this->hasMany('app\THUOC','ID_KM','ID_KM');
+    }
 }
