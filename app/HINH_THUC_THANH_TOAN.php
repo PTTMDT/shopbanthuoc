a9665cbe01fa5f25b62a26ba555e23e0bf5377fd
lobe520 <64627346+lobe520@users.noreply.github.com>
2020-05-01 13:09:33 +0700

    Update HINH_THUC_THANH_TOAN.php

--- app/HINH_THUC_THANH_TOAN.php
+++ app/HINH_THUC_THANH_TOAN.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class HINH_THUC_THANH_TOAN extends Model
 {
     //
+    protected $table = "HINH_THUC_THANH_TOAN";
+    
+    
+    public function DON_DAT_HANG()
+    {
+        return $this->hasMany('App\THUOC','ID_HT','ID_HT')
+    }
 }
