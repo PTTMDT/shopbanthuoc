d75bcf689316e9543c2c9a3525a34ab1bbdb49b4
lobe520 <64627346+lobe520@users.noreply.github.com>
2020-05-01 13:00:48 +0700

    Update LO.php

--- app/LO.php
+++ app/LO.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class LO extends Model
 {
     //
+    protected $table  = "LO";
+    public function LOAI_LO()
+    {
+        return $this->belongsTo('App\LOAI_LO','ID_LOAI_LO','ID_LO')
+    }
+    public function NHA_CUNG_CAP()
+    {
+        return $this->belongsTo('App\NHA_CUNG_CAP','ID_NCC','ID_LO')
+
+    }
+    public function CHI_TIET_LO()
+
+    {
+        return $this->hasMany('App\CHI_TIET_LO','ID_LO','ID_LO')
+    }
 }
