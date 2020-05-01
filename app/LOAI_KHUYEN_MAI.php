a1cedf500d192862fe3977a786f6ef39ae5971f8
nguyenthiy <63966829+nguyenthiy@users.noreply.github.com>
2020-05-01 12:51:58 +0700

    Update LOAI_KHUYEN_MAI.php

--- app/LOAI_KHUYEN_MAI.php
+++ app/LOAI_KHUYEN_MAI.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class LOAI_KHUYEN_MAI extends Model
 {
     //
+        protected $table = "LOAI_KHUYEN_MAI";
+    public function KHUYEN_MAI(){
+        return $this->hasMany('app\KHUYEN_MAI','ID_LOAI_KM','ID_LOAI_KM');
+    }
 }
