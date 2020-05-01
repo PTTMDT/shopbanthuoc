47c43f0a655717049a7f19dbed28ebcf52f7ccbc
tttnguyenhttt <64627323+tttnguyenhttt@users.noreply.github.com>
2020-05-01 13:04:30 +0700

    Update LOAI_LO.php

--- app/LOAI_LO.php
+++ app/LOAI_LO.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class LOAI_LO extends Model
 {
     //
+    protected $table = "LOAI_LO";
+	 public function LO(){
+       return $this->hasMany('app\LO','ID_LO');
+  }
 }
