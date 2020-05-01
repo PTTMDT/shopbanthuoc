1c0d5ed532200ea6c49bd8a93026c5c6041a28dd
lobe520 <64627346+lobe520@users.noreply.github.com>
2020-05-01 13:02:29 +0700

    Update CHI_TIET_LO.php

--- app/CHI_TIET_LO.php
+++ app/CHI_TIET_LO.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class CHI_TIET_LO extends Model
 {
     //
+     protected $table = "CHI_TIET_LO";
+    
+    public function LO ()
+    {
+        return $this->belongsTo('App\LO','ID_LO','ID_LO')
+    }
+    public function THUOC()
+    {
+        return $this->belongsTo('App\THUOC','ID_THUOC','ID_THUOC')
+    }
 }
