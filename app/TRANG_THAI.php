938a9ee6fd6b485d1b747a14b0b8cd87a475bc64
nguyenthiy <63966829+nguyenthiy@users.noreply.github.com>
2020-05-01 13:11:58 +0700

    Update TRANG_THAI.php

--- app/TRANG_THAI.php
+++ app/TRANG_THAI.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class TRANG_THAI extends Model
 {
     //
+    protected $table = "TRANG_THAI";
+    public function DON_DAT_HANG(){
+        return $this->hasMany('app\DON_DAT_HANG','ID_TT','ID_TT');
+    }
 }
