9a3c6bcf24bca63a81276c9b72fcb9550c5b7c3d
nguyenthiy <63966829+nguyenthiy@users.noreply.github.com>
2020-05-01 12:51:20 +0700

    Update LOAI_NHAN_VIEN.php

--- app/LOAI_NHAN_VIEN.php
+++ app/LOAI_NHAN_VIEN.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class LOAI_NHAN_VIEN extends Model
 {
     //
+        protected $table = "LOAI_NHAN_VIEN";
+    public function NHAN_VIEN(){
+        return $this->hasMany('app\NHAN_VIEN','ID_LOAI','ID_LOAI');
+    }
 }
