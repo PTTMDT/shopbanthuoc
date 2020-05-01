021f0a9bf5f795870965a4c7806fdda0b34ed7e7
lobe520 <64627346+lobe520@users.noreply.github.com>
2020-05-01 13:13:10 +0700

    Update HINH_THUC_VAN_CHUYEN.php

--- app/HINH_THUC_VAN_CHUYEN.php
+++ app/HINH_THUC_VAN_CHUYEN.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class HINH_THUC_VAN_CHUYEN extends Model
 {
     //
+    protected $table = "HINH_THUC_VAN_CHUYEN";
+    
+    
+    public function DON_DAT_HANG()
+    {
+        return $this->hasMany('App\DON_DAT_HANG','ID_VC','ID_VC')
+    }
 }
