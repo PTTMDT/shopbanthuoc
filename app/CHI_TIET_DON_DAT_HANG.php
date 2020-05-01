6e259e68a4d7a156c5fb7a52a33fefee2a003275
tttnguyenhttt <64627323+tttnguyenhttt@users.noreply.github.com>
2020-05-01 13:00:46 +0700

    Update CHI_TIET_DON_DAT_HANG.php

--- app/CHI_TIET_DON_DAT_HANG.php
+++ app/CHI_TIET_DON_DAT_HANG.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class CHI_TIET_DON_DAT_HANG extends Model
 {
     //
+    protected $table ="CHI_TIET_DON_DAT_HANG";
+    public function THUOC(){
+       return $this->belongsTo('app\THUOC','ID_THUOC','ID_THUOC');
+  }
+     public function DON_DAT_HANG(){
+       return $this->belongsTo('app\THUOC','ID_DDH','ID_DDH');
+  }
 }
