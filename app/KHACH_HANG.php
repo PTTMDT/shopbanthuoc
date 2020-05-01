7bdf7b4f41f23223c0ef9efd38362b051dbc9853
tttnguyenhttt <64627323+tttnguyenhttt@users.noreply.github.com>
2020-05-01 13:12:57 +0700

    Update KHACH_HANG.php

--- app/KHACH_HANG.php
+++ app/KHACH_HANG.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class KHACH_HANG extends Model
 {
     //
+    protected $table = "KHACH_HANG";
+	 public function DANH_GIA(){
+       return $this->hasMany('app\DANH_GIA','ID_KH','ID_KH');
+  }
+	 public function DON_DAT_HANG(){
+       return $this->hasMany('app\DON_DAT_HANG','ID_DDH');
+  }
+	 public function LOAI_KH(){
+       return $this->belongsTo('app\LOAI_KH','ID_LKH');
+  }
 }
