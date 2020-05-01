fc799df3672d3f6bd78c1bf45dd4b8450188d9b8
tttnguyenhttt <64627323+tttnguyenhttt@users.noreply.github.com>
2020-05-01 13:03:32 +0700

    Update DANH_GIA.php

--- app/DANH_GIA.php
+++ app/DANH_GIA.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class DANH_GIA extends Model
 {
     //
+    protected $table = "DANH_GIA";
+	 public function THUOC(){
+       return $this->belongsTo('app\THUOC','ID_THUOC');
+  }
+	 public function KHACH_HANG(){
+       return $this->belongsTo('app\KHACH_HANG','ID_KH');
+  }
 }
