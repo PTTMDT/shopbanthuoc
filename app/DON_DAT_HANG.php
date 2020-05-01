c4bbbb6f49fcbf0c00c6f10727a20486e9bfdb2c
tttnguyenhttt <64627323+tttnguyenhttt@users.noreply.github.com>
2020-05-01 13:01:52 +0700

    Update DON_DAT_HANG.php

--- app/DON_DAT_HANG.php
+++ app/DON_DAT_HANG.php
@@ -7,4 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class DON_DAT_HANG extends Model
 {
     //
+    protected $table = "DON_DAT_HANG";
+	public function KHUYEN_MAI(){
+        return $this->belongsTo('app\KHUYEN_MAI','ID_KM');
+  }
+	 public function NHAN_VIEN(){
+       return $this->belongsTo('app\NHAN_VIEN','ID_NV');
+  }
+	 public function TIEN_TE(){
+       return $this->belongsTo('app\TIEN_TE','ID_TIENTE');
+  }
+	 public function HINH_THUC_TT(){
+       return $this->belongsTo('app\HINH_THUC_TT','ID_HT');
+  }
+ 	public function HINH_THUC_VC(){
+       return $this->belongsTo('app\HINH_THUC_VC','ID_VC');
+  }
+	 public function TRANG_THAI(){
+       return $this->belongsTo('app\TRANG_THAI','ID_TT');
+  }
+	 public function KHACH_HANG(){
+       return $this->belongsTo('app\KHACH_HANG','ID_KH');
+  }
+	 public function CHI_TIET_DDH(){
+       return $this->hasMany('app\CHI_TIET_DDH','ID_DDH','ID_DDH');
+  }
 }
