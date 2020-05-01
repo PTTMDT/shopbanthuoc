4a99292ec10a483b7c7b45d1b8f44c51bfd3192b
nguyenthiy <63966829+nguyenthiy@users.noreply.github.com>
2020-05-01 12:52:33 +0700

    Update LOAI_KHACH_HANG.php

--- app/LOAI_KHACH_HANG.php
+++ app/LOAI_KHACH_HANG.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class LOAI_KHACH_HANG extends Model
 {
     //
+        protected $table = "LOAI_KHACH_HANG";
+    public function KHACH_HANG(){
+        return $this->hasMany('app\KHACH_HANG','ID_LKH','ID_LKH');
+    }
 }
