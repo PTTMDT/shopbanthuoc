3197f7bffc834048c6cdff3acc1daaa58d25d3a6
nguyenthiy <63966829+nguyenthiy@users.noreply.github.com>
2020-05-01 12:47:54 +0700

    Update TIEN_TE.php

--- app/TIEN_TE.php
+++ app/TIEN_TE.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class TIEN_TE extends Model
 {
     //
+    protected $table = "LOAI_NHAN_VIEN";
+    public function NHAN_VIEN(){
+        return $this->hasMany('app\NHAN_VIEN','ID_LOAI','ID_LOAI');
+    }
 }
