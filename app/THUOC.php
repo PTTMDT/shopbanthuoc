8712341e59e37feb34f0ac695637a1e212171dc8
ThuThaoNguyen <57536071+ThuThaoNguyen@users.noreply.github.com>
2020-05-01 13:16:08 +0700

    Update THUOC.php

--- app/THUOC.php
+++ app/THUOC.php
@@ -7,6 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class THUOC extends Model
 {
     //
-    protected $table = "THUOC";
-    // public function 
+  protected $table = "THUOC";
+    public $timestamp= false;
+    public function GOC_THUOC(){
+        return $this->belongsTo('app\GOC_THUOC','ID_GOC','ID_THUOC');
+    }
+       public function DON_VI_TINH(){
+        return $this->belongsTo('app\DON_VI_TINH','DVT','ID_THUOC');
+    }
+       public function CHI_TIET_LO(){
+        return $this->hasMany('app\CHI_TIET_LO','ID_THUOC','ID_THUOC');
+    }
+       public function GOC_THUOC(){
+        return $this->belongsTo('app\KHUYEN_MAI','ID_KM','ID_THUOC');
+    }  
+       public function CHI_TIET_DDH(){
+        return $this->hasMany('app\CHI_TIET_DDH','ID_THUOC','ID_THUOC');
+    }
+       public function DANH_GIA(){
+        return $this->hasMany('app\DANH_GIA','ID_THUOC','ID_THUOC');
+    }
+    
 }
