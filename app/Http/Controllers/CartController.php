3ece88c779bc6e928b70adc8d8d6e71df3d64592
= <=>
2020-05-03 18:11:56 +0700

    update gio hang

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -15,15 +15,15 @@ class CartController extends Controller
     public function save_cart(Request $request){
     	$productId = $request->productid_hidden;
     	$quantity = $request->qty;
-    	$product_info = DB::table('tbl_product')->where('product_id',$productId)->first(); 
+    	$product_info = DB::table('thuoc')->where('ID_THUOC',$productId)->first(); 
 
     
         // Cart::add('293ad', 'Product 1', 1, 9.99, 550);
         // Cart::destroy();
-        $data['id'] = $product_info->product_id;
+        $data['id'] = $product_info->ID_THUOC;
         $data['qty'] = $quantity;
-        $data['name'] = $product_info->product_name;
-        $data['price'] = $product_info->product_price;
+        $data['name'] = $product_info->TEN_THUOC;
+        $data['price'] = $product_info->DON_GIA;
         $data['weight'] = $product_info->product_price;
         $data['options']['image'] = $product_info->product_image;
         Cart::add($data);
@@ -33,8 +33,8 @@ class CartController extends Controller
     }
     public function show_cart(){
 
-        $cate_product = DB::table('tbl_category_product')->where('category_status','0')->orderby('category_id','desc')->get(); 
-        $brand_product = DB::table('tbl_brand')->where('brand_status','0')->orderby('brand_id','desc')->get(); 
+        $cate_product = DB::table('goc_thuoc')->where('category_status','0')->orderby('ID_GOC','desc')->get(); 
+       $brand_product = DB::table('nha_cung_cap')->where('brand_status','0')->orderby('ID_NCC','desc')->get(); 
         return view('pages.cart.show_cart')->with('category',$cate_product)->with('brand',$brand_product);
     }
     public function delete_to_cart($rowId){
