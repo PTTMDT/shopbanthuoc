94090f076c8bcc30522c7287c0300a755c942948
tttnguyenhttt <64627323+tttnguyenhttt@users.noreply.github.com>
2020-05-12 10:57:55 +0700

    Update add_brand_product.blade.php

--- resources/views/admin/add_brand_product.blade.php
+++ resources/views/admin/add_brand_product.blade.php
@@ -4,7 +4,7 @@
             <div class="col-lg-12">
                     <section class="panel">
                         <header class="panel-heading">
-                           Thêm thương hiệu sản phẩm
+                           Thêm nhà cung cấp
                         </header>
                          <?php
                             $message = Session::get('message');
@@ -19,16 +19,27 @@
                                 <form role="form" action="{{URL::to('/save-brand-product')}}" method="post">
                                     {{ csrf_field() }}
                                 <div class="form-group">
-                                    <label for="exampleInputEmail1">Tên thương hiệu</label>
+                                    <label for="exampleInputEmail1">Tên nhà cung cấp</label>
                                     <input type="text" name="TEN_NCC" class="form-control" id="exampleInputEmail1" placeholder="Tên danh mục">
                                 </div>
                                 <div class="form-group">
-                                    <label for="exampleInputEmail1">Slug</label>
-                                    <input type="text" name="brand_slug" class="form-control" id="exampleInputEmail1" placeholder="Slug">
+                                    <label for="exampleInputEmail1">Số điện thoại nhà cung cấp</label>
+                                    <input type="text" name="SDT_NCC" class="form-control" id="exampleInputEmail1" placeholder="Tên danh mục">
                                 </div>
                                 <div class="form-group">
-                                    <label for="exampleInputPassword1">Mô tả thương hiệu</label>
-                                    <textarea style="resize: none" rows="8" class="form-control" name="brand_product_desc" id="exampleInputPassword1" placeholder="Mô tả danh mục"></textarea>
+                                    <label for="exampleInputPassword1">Địa chỉ nhà cung cấp</label>
+                                    <textarea style="resize: none" rows="8" class="form-control" name="DC_NCC" id="exampleInputPassword1" placeholder="Mô tả danh mục"></textarea>
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">BRAND SLUG</label>
+                                    <input type="text" name="slug" class="form-control" id="exampleInputEmail1" placeholder="Tên danh mục">
+                                </div><div class="form-group">
+                                    <label for="exampleInputEmail1">BRAND DESC</label>
+                                    <input type="text" name="desc" class="form-control" id="exampleInputEmail1" placeholder="Tên danh mục">
+                                </div><div class="form-group">
+                                    <label for="exampleInputEmail1">BRAND STATUS</label>
+                                    <input type="text" name="status" class="form-control" id="exampleInputEmail1" placeholder="Tên danh mục">
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputPassword1">Hiển thị</label>
@@ -39,7 +50,7 @@
                                     </select>
                                 </div>
                                
-                                <button type="submit" name="add_category_product" class="btn btn-info">Thêm thương hiệu</button>
+                                <button type="submit" name="add_category_product" class="btn btn-info">Thêm nhà cung cấp</button>
                                 </form>
                             </div>
 
