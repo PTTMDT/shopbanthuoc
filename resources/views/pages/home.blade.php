d67d3ad369e2566775da551d0b3295355e52d3f3
nguyenthiy <63966829+nguyenthiy@users.noreply.github.com>
2020-05-12 10:33:47 +0700

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('content')
 <div class="features_items"><!--features_items-->
-                        <h2 class="title text-center">Sản phẩm mới nhất</h2>
+                        <h2 class="title text-center" style="color: #008B8B">Sản phẩm mới nhất</h2>
                         @foreach($all_product as $key => $product)
                         <div class="col-sm-4">
                             <div class="product-image-wrapper">
@@ -9,9 +9,9 @@
                                 <div class="single-products">
                                         <div class="productinfo text-center">
                                             <img src="{{URL::to('public/uploads/product/'.$product->HINH_ANH)}}" alt="" />
-                                            <h2>{{number_format($product->DON_GIA).' '.'VNĐ'}}</h2>
+                                            <h2 style="color: #008B8B ">{{number_format($product->DON_GIA).' '.'VNĐ'}}</h2>
                                             <p>{{$product->TEN_THUOC}}</p>
-                                            <a href="{{URL::to('/chi-tiet-san-pham/'.$product->product_slug)}}" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm giỏ hàng</a>
+                                            <a href="{{URL::to('/chi-tiet-san-pham/'.$product->product_slug)}}" class="btn btn-default add-to-cart" style="background-color: #008B8B;color: white"><i class="fa fa-shopping-cart"></i>Thêm giỏ hàng</a>
                                         </div>
                                       
                                 </div>
