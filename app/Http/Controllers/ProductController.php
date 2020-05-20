54123bbf8decdca538da9a3cafa7dcea6a2cc132
ThuThaoNguyen <57536071+ThuThaoNguyen@users.noreply.github.com>
2020-05-20 23:31:05 +0700

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use DB;
 use Session;
 use App\Http\Requests;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Redirect;
 session_start();
 class ProductController extends Controller
