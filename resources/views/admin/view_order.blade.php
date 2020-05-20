01491940946528c18998f741eae5f33bf73611e9
lobe520 <64627346+lobe520@users.noreply.github.com>
2020-05-20 16:07:36 +0700

    Update view_order.blade.php

--- resources/views/admin/view_order.blade.php
+++ resources/views/admin/view_order.blade.php
@@ -50,7 +50,7 @@
   
   <div class="panel panel-default">
     <div class="panel-heading">
-     Thông tin vận chuyển
+     Thông tin đơn đặt hàng
     </div>
     
     
@@ -66,9 +66,90 @@
         <thead>
           <tr>
            
-            <th>Hình thức vận chuyển</th>
-            <th>Giá vận chuyển</th>
+            <th>Tổng đơn đặt hàng:</th>
+            <td>{{$order_by_id->TONG_DDH}}</td>
+          
             
+            <th style="width:30px;"></th>
+          </tr>
+        </thead>
+        <thead>
+          <tr>
+           
+            <th>Ngày đặt:</th>
+            <td>{{$order_by_id->NGAY_DAT}}</td>
+          
+            
+            <th style="width:30px;"></th>
+          </tr>
+        </thead>
+        <thead>
+          <tr>
+           
+            <th>Nhân viên giao dịch:</th>
+            <td>{{$order_by_id->TEN_NV}}</td>
+            <th>Số điện thoại:</th>
+            <td>{{$order_by_id->SDT}}</td>
+            
+            <th style="width:30px;"></th>
+          </tr>
+        </thead>
+        
+
+        <thead>
+        
+          <tr>
+           
+            <th>Hình thức vận chuyển:</th>
+            <td>{{$order_by_id->TEN_VC}}</td>
+            <th>Giá vận chuyển:</th>
+            <td>{{$order_by_id->GIA_VC}}</td>
+             
+            
+           
+          
+          </tr>
+     
+        </tbody>
+        <thead>
+          <tr>
+           
+            <th>Hình thức thanh_toán</th>
+            <td>{{$order_by_id->TEN_HT}}</td>
+            
+            <th style="width:30px;"></th>
+          </tr>
+        </thead>
+      </table>
+
+    </div>
+   
+  </div>
+</div>
+<br><br>
+<div class="table-agile-info">
+  
+  <div class="panel panel-default">
+    <div class="panel-heading">
+     Chi tiết đơn hàng
+    </div>
+    
+    
+    <div class="table-responsive">
+                      <?php
+                            $message = Session::get('message');
+                            if($message){
+                                echo '<span class="text-alert">'.$message.'</span>';
+                                Session::put('message',null);
+                            }
+                            ?>
+      <table class="table table-striped b-t b-light">
+        <thead>
+          <tr>
+           
+            <th>Tên thuốc</th>
+            <th>Số lượng</th>
+            <th>Đơn giá</th>
           
             
             <th style="width:30px;"></th>
@@ -77,10 +158,10 @@
         <tbody>
         
           <tr>
-           
-            <td>{{$order_by_id->TEN_VC}}</td>
-            <td>{{$order_by_id->GIA_VC}}</td>
-             
+        
+          <td>{{$order_by_id->TEN_THUOC}}</td>
+          <td>{{$order_by_id->SO_LUONG}}</td>
+          <td>{{$order_by_id->DON_GIA}}</td>
             
            
           
@@ -93,94 +174,6 @@
    
   </div>
 </div>
-<br><br>
-<div class="table-agile-info">
-  
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      Liệt kê chi tiết đơn hàng
-    </div>
-    <div class="row w3-res-tb">
-      <div class="col-sm-5 m-b-xs">
-        <select class="input-sm form-control w-sm inline v-middle">
-          <option value="0">Hoạt động</option>
-          <option value="1">Xóa</option>
-          <option value="2">Chỉnh sửa</option>
-          <option value="3">Xuất</option>
-        </select>
-        <button class="btn btn-sm btn-default">Thực hiện</button>                
-      </div>
-      <div class="col-sm-4">
-      </div>
-      <div class="col-sm-3">
-        <div class="input-group">
-          <input type="text" class="input-sm form-control" placeholder="Search">
-          <span class="input-group-btn">
-            <button class="btn btn-sm btn-default" type="button">Tìm</button>
-          </span>
-        </div>
-      </div>
-    </div>
-    
-    <div class="table-responsive">
-                      <?php
-                            $message = Session::get('message');
-                            if($message){
-                                echo '<span class="text-alert">'.$message.'</span>';
-                                Session::put('message',null);
-                            }
-                            ?>
-      <table class="table table-striped b-t b-light">
-        <thead>
-          <tr>
-            <th style="width:20px;">
-              <label class="i-checks m-b-none">
-                <input type="checkbox"><i></i>
-              </label>
-            </th>
-            <th>Tên sản phẩm</th>
-            <th>Số lượng</th>
-            <th>Giá</th>
-            <th>Tổng tiền</th>
-            
-            <th style="width:30px;"></th>
-          </tr>
-        </thead>
-        <tbody>
-         
-          <tr>
-           
-            <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
-            <td>{{$order_by_id->TEN_THUOC}}</td>
-            <td>{{$order_by_id->SO_LUONG}}</td>
-            <td>{{$order_by_id->DON_GIA}}</td>
-            <td>{{$order_by_id->DON_GIA*$order_by_id->SO_LUONG}}</td>
-          
-           
-          </tr>
-         
-        </tbody>
-      </table>
+<br>
 
-    </div>
-    <footer class="panel-footer">
-      <div class="row">
-        
-        <div class="col-sm-5 text-center">
-          <small class="text-muted inline m-t-sm m-b-sm">showing 20-30 of 50 items</small>
-        </div>
-        <div class="col-sm-7 text-right text-center-xs">                
-          <ul class="pagination pagination-sm m-t-none m-b-none">
-            <li><a href=""><i class="fa fa-chevron-left"></i></a></li>
-            <li><a href="">1</a></li>
-            <li><a href="">2</a></li>
-            <li><a href="">3</a></li>
-            <li><a href="">4</a></li>
-            <li><a href=""><i class="fa fa-chevron-right"></i></a></li>
-          </ul>
-        </div>
-      </div>
-    </footer>
-  </div>
-</div>
 @endsection
