e8c21fe3dcbdf61de605e2e072d8f8f229d0bfda
tuyetlan2704 <63777003+tuyetlan2704@users.noreply.github.com>
2020-05-20 16:56:30 +0700

    Update all_lo_product.blade.php

--- resources/views/admin/all_lo_product.blade.php
+++ resources/views/admin/all_lo_product.blade.php
@@ -1,74 +1,115 @@
 @extends('admin_layout')
 @section('admin_content')
-<div class="row">
-            <div class="col-lg-12">
-                    <section class="panel">
-                        <header class="panel-heading">
-                           Thêm lô
-                        </header>
-                         <?php
+    <div class="table-agile-info">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      Liệt kê lô
+    </div>
+    <div class="row w3-res-tb">
+      <div class="col-sm-5 m-b-xs">
+        <select class="input-sm form-control w-sm inline v-middle">
+          <option value="0">Bulk action</option>
+          <option value="1">Delete selected</option>
+          <option value="2">Bulk edit</option>
+          <option value="3">Export</option>
+        </select>
+        <button class="btn btn-sm btn-default">Apply</button>                
+      </div>
+      <div class="col-sm-4">
+      </div>
+      <div class="col-sm-3">
+        <div class="input-group">
+          <input type="text" class="input-sm form-control" placeholder="Search">
+          <span class="input-group-btn">
+            <button class="btn btn-sm btn-default" type="button">Go!</button>
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="table-responsive">
+                      <?php
                             $message = Session::get('message');
                             if($message){
                                 echo '<span class="text-alert">'.$message.'</span>';
                                 Session::put('message',null);
                             }
                             ?>
-                        <div class="panel-body">
-
-                            <div class="position-center">
-                                <form role="form" action="{{URL::to('/savelo-product')}}" method="post">
-                                    {{ csrf_field() }}
-
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Nhà cung cấp</label>
-                                    <input type="text" name="id_ncc" class="form-control" id="exampleInputEmail1" placeholder="Lô">
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Loại lô</label>
-                                      <select name="id_loai_lo" class="form-control input-sm m-bot15">
-                                        @foreach($cate_product as $key => $cate)
-                                            <option value="{{$cate->ID_LOAI_LO}}">{{$cate->TEN_LOAI_LO}}</option>
-                                        @endforeach  
-                                    </select>
-                                </div>
-
+      <table class="table table-striped b-t b-light">
+        <thead>
+          <tr>
+            <th style="width:20px;">
+              <label class="i-checks m-b-none">
+                <input type="checkbox"><i></i>
+              </label>
+            </th>
+            <th>Tên lô</th>
+           
+            <th>Ngày sản xuất</th>
+            <th>Ngày hết hạn</th>
+            <th>Ngày nhập</th>
+            <th>Hiển thị</th>
+            
+            <th style="width:30px;"></th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($alllo_product as $key => $pro)
+          <tr>
+            <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
+            
                                
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Ngày sản xuất</label>
-                                    <input type="date"  name="ngaysx" class="form-control" id="exampleInputEmail1" placeholder="Lô">
-                                </div>
-                                
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Ngày hết hạn</label>
-                                    <input type="date" name="ngayhh" class="form-control" id="exampleInputEmail1" placeholder="Lô">
-                                </div>
+            
+            <td>{{ $pro->TEN_LOAI_LO}}</td>
+            
+            <td>{{ $pro->NGAY_SX }}</td>
+            <td>{{ $pro->NGAY_HH}}</td>
+            <td>{{ $pro->NGAY_NHAP}}</td>
+            <td>{{ $pro->LO_status}}</td>
 
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Ngày nhập</label>
-                                    <input type="date" name="ngaynhap" class="form-control" id="exampleInputEmail1" placeholder="Lô">
-                                </div>
-
-                              
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">LO_status</label>
-                                    <input type="text" value="0"name="lo_status" class="form-control" id="exampleInputEmail1" placeholder="Lô">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Hiển thị</label>
-                                      <select name="brand_product_status" class="form-control input-sm m-bot15">
-                                            <option value="0">Ẩn</option>
-                                            <option value="1">Hiển thị</option>
-                                            
-                                    </select>
-                                </div>
-                               
-                                <button type="submit" name="add_category_product" class="btn btn-info">Thêm lô</button>
-                                </form>
-                            </div>
-
-                        </div>
-                    </section>
-
-            </div>
+            <td><span class="text-ellipsis">
+              <?php
+               if($pro->LO_status==0){
+                ?>
+                <a href="{{URL::to('/unactivelo-product/'.$pro->ID_LO)}}"><span class="fa-thumb-styling fa fa-thumbs-up"></span></a>
+                <?php
+                 }else{
+                ?>  
+                 <a href="{{URL::to('/activelo-product/'.$pro->ID_LO)}}"><span class="fa-thumb-styling fa fa-thumbs-down"></span></a>
+                <?php
+               }
+              ?>
+            </span></td>
+           
+            <td>
+              <a href="{{URL::to('/editlo-product/'.$pro->ID_LO)}}" class="active styling-edit" ui-toggle-class="">
+                <i class="fa fa-pencil-square-o text-success text-active"></i></a>
+              <a onclick="return confirm('Bạn có chắc là muốn xóa lô này ko?')" href="{{URL::to('/deletelo-product/'.$pro->ID_LO)}}" class="active styling-edit" ui-toggle-class="">
+                <i class="fa fa-times text-danger text"></i>
+              </a>
+            </td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+    <footer class="panel-footer">
+      <div class="row">
+        
+        <div class="col-sm-5 text-center">
+          <small class="text-muted inline m-t-sm m-b-sm">showing 20-30 of 50 items</small>
+        </div>
+        <div class="col-sm-7 text-right text-center-xs">                
+          <ul class="pagination pagination-sm m-t-none m-b-none">
+            <li><a href=""><i class="fa fa-chevron-left"></i></a></li>
+            <li><a href="">1</a></li>
+            <li><a href="">2</a></li>
+            <li><a href="">3</a></li>
+            <li><a href="">4</a></li>
+            <li><a href=""><i class="fa fa-chevron-right"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  </div>
+</div>
 @endsection
