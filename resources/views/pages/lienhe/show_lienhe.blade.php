50cfe5bb43aeeee9c38dc175d1d47ab64303d26b
tuyetlan2704 <63777003+tuyetlan2704@users.noreply.github.com>
2020-05-25 16:01:42 +0700

    Add files via upload

---
+++ resources/views/pages/lienhe/show_lienhe.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('content')
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31430.628112721188!2d105.75139442604087!3d10.03100590094513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0883d0dac6b15%3A0xf6ae5b1bd18625!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBD4bqnbiBUaMah!5e0!3m2!1svi!2sus!4v1590388330513!5m2!1svi!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> -->
+@endsection
