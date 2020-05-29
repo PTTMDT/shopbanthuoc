dea31b16493dc90f5d759e7e29d33d4cda5d02fe
lobe520 <64627346+lobe520@users.noreply.github.com>
2020-05-29 22:18:17 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -108,6 +108,7 @@ Route::post('/save-checkout-customer','CheckoutController@save_checkout_customer
 //Order
 Route::get('/manage-order','CheckoutController@manage_order');
 Route::get('/view-order/{orderId}','CheckoutController@view_order');
+Route::post('/update-order/{orderId}','CheckoutController@update_order');
 Route::get('/print-order/{checkout_code}','CheckoutController@print_order');
 // LO
 Route::get('/add-lo-product','LoController@addlo_product');
