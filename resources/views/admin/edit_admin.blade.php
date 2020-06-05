7be1d659f421feccaf58d3dd87df5291bc95545a
lobe520 <64627346+lobe520@users.noreply.github.com>
2020-06-05 21:01:17 +0700

    Add files via upload

---
+++ resources/views/admin/edit_admin.blade.php
@@ -0,0 +1,56 @@
+@extends('admin_layout')
+@section('admin_content')
+<div class="row">
+            <div class="col-lg-12">
+                    <section class="panel">
+                        <header class="panel-heading">
+                           Cập nhật nhân viên
+                        </header>
+                         <?php
+                            $message = Session::get('message');
+                            if($message){
+                                echo '<span class="text-alert">'.$message.'</span>';
+                                Session::put('message',null);
+                            }
+                            ?>
+                        <div class="panel-body">
+
+                            <div class="position-center">
+                                @foreach($edit_product as $key => $pro)
+                                <form role="form" action="{{URL::to('/update-admin/'.$pro->ID_NV)}}" method="post" enctype="multipart/form-data">
+                                    {{ csrf_field() }}
+                                 <div class="form-group">
+                                    <label for="exampleInputEmail1">Tên nhân viên</label>
+                                    <input type="text" name="ten_nv" class="form-control" id="exampleInputEmail1" value="{{$pro->TEN_NV}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Số điện thoại</label>
+                                    <input type="text" name="sdt" class="form-control" id="exampleInputEmail1" value="{{$pro->SDT}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Email</label>
+                                    <input type="text" name="email_nv" class="form-control" id="exampleInputEmail1" value="{{$pro->EMAIL_NV}}">
+                                </div>
+                                <div class="form-group">
+                                    <!-- <label for="exampleInputEmail1"></label> -->
+                                    <input type="hidden" name="id_loai" class="form-control" id="exampleInputEmail1" value="{{$pro->ID_LOAI}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Hiển thị</label>
+                                      <select name="product_status" class="form-control input-sm m-bot15">
+                                            <option value="0">Ẩn</option>
+                                            <option value="1">Hiển thị</option>
+                                            
+                                    </select>
+                                </div>
+                               
+                                <button type="submit" name="add_product" class="btn btn-info">Cập nhật nhân viên<nav></nav></button>
+                                </form>
+                                @endforeach
+                            </div>
+
+                        </div>
+                    </section>
+
+            </div>
+@endsection
